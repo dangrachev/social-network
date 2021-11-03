03317c6b2783f06cb035e30566cadf158bd16cfb
undfned <jr9160260034@gmail.com>
2021-11-03 18:40:39 +0300

    Edited Profile and Messages, added new component

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,10 +1,35 @@
 import style from './Messages.module.css'
+import {NavLink} from "react-router-dom";
 
+const DialogItem = (props) => {
+    let path = `/messages/${props.id}`;
+
+    return(
+        <div className={style.dialogs_item}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    );
+}
+
+const Message = (props) => {
+    return <div className={style.messages_item}>{props.message}</div>
+}
 
 const Messages = () => {
     return (
         <div className={style.messages_wrapper}>
-            <h1>Messages</h1>
+            <div className={style.dialogs}>
+                <DialogItem name='Kathrine' id='1'/>
+                <DialogItem name='Joel' id='2'/>
+                <DialogItem name='Rick' id='3'/>
+                <DialogItem name='Alice' id='4'/>
+            </div>
+            <div className={style.messages}>
+                <Message message='WUBBA-LUBBA-DUB-DUB'/>
+                <Message message='WUBBA-LUBBA-DUB-DUB'/>
+                <Message message='WUBBA-LUBBA-DUB-DUB'/>
+                <Message message='WUBBA-LUBBA-DUB-DUB'/>
+            </div>
         </div>
     );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,26 +1,12 @@
 import style from './Profile.module.css'
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import Posts from './Posts/Posts';
 
 
 const Profile = () => {
     return (
         <div className={style.profile_wrapper}>
-            <div className={style.template_img__wrapper}>
-                <img className={style.template_img} src="https://wallpaperaccess.com/full/1282257.jpg" alt="template"/>
-            </div>
-            <div className={style.profile_info}>
-                <div className={style.avatar_wrapper}>
-                    <div className={style.avatar}></div>
-                    <div className={style.userName}>Elon Musk</div>
-                </div>
-                <div className={style.profile_description}>
-                    <ul className={style.user_info}>
-                        <li className={style.info_item}>city: Boca-chica, Texas</li>
-                        <li className={style.info_item}>age: 50</li>
-                        <li className={style.info_item}>SpaceX, Tesla, Neuralink, the Borning Company</li>
-                    </ul>
-                </div>
-            </div>
+            <ProfileInfo />
             <Posts />
         </div>
     );
