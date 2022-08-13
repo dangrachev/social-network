11e6a8404f496d8c645de732ad45df8e1450b884
undfned <jr9160260034@gmail.com>
2022-08-13 15:56:21 +0300

    React.lazy & Suspense

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {Route, Switch, withRouter} from 'react-router-dom';
 import './App.css';
-import ProfileContainer from './components/Profile/ProfileContainer';
-import Feeds from './components/Feeds/Feeds';
-import Music from './components/Music/Music';
-import Settings from './components/Settings/Settings';
-import MessagesContainer from './components/Messages/MessagesContainer';
-import NavbarContainer from './components/Navbar/NavbarContainer';
-import UsersContainer from './components/Users/UsersContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
+import ProfileContainer from './components/Profile/ProfileContainer';
+import NavbarContainer from './components/Navbar/NavbarContainer';
 import Login from './components/Login/Login';
+import Feeds from './components/Feeds/Feeds';
+import Settings from './components/Settings/Settings';
+import Preloader from './components/common/Preloader/Preloader';
 import {compose} from 'redux';
 import {connect} from 'react-redux';
 import {initializeApp} from './Redux/app-reducer';
-import Preloader from './components/common/Preloader/Preloader';
 //import Footer from './components/Footer/Footer';
+
+
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
+const MessagesContainer = React.lazy(() => import('./components/Messages/MessagesContainer'));
+const Music = React.lazy(() => import('./components/Music/Music'));
 
 class App extends React.Component {
 
@@ -31,15 +33,17 @@ class App extends React.Component {
                 <HeaderContainer/>
                 <NavbarContainer/>
                 <div className="content_wrapper">
-                    <Switch>
-                        <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
-                        <Route path='/messages' render={() => <MessagesContainer/>}/>
-                        <Route path='/users' render={() => <UsersContainer/>}/>
-                        <Route path='/feeds' render={() => <Feeds/>}/>
-                        <Route path='/music' render={() => <Music/>}/>
-                        <Route path='/settings' render={() => <Settings/>}/>
-                        <Route path='/login' render={() => <Login/>}/>
-                    </Switch>
+                    <Suspense fallback={<Preloader/>}>
+                        <Switch>
+                            <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                            <Route path='/messages' render={() => <MessagesContainer/>}/>
+                            <Route path='/users' render={() => <UsersContainer/>}/>
+                            <Route path='/music' render={() => <Music/>}/>
+                            <Route path='/feeds' render={() => <Feeds/>}/>
+                            <Route path='/settings' render={() => <Settings/>}/>
+                            <Route path='/login' render={() => <Login/>}/>
+                        </Switch>
+                    </Suspense>
                 </div>
             </div>
         );
